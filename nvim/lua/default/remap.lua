2f8d359576c0e25a88f26db2dcf3f379cd51c4a1
deanqx <dean2@kowatsch.de>
2024-01-10 20:11:56 +0100

    New keybinds

--- nvim/lua/default/remap.lua
+++ nvim/lua/default/remap.lua
@@ -1,10 +1,12 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>o", ":vsplit<CR>")
+vim.keymap.set("n", "<leader>s", ":vsplit<CR>")
 
--- Line moving
+-- Lines
 vim.keymap.set("v", "J", ":move '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":move '<-2<CR>gv=gv")
+vim.keymap.set("n", "<leader>o", "o <ESC>")
+vim.keymap.set("n", "<leader>O", "O <ESC>")
 
 -- Lock cursor
 vim.keymap.set("n", "J", "mzJ`z")
@@ -36,5 +38,5 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Lefft><Left><Left>")
+--- vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Lefft><Left><Left>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
