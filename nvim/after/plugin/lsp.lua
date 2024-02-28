94618e76f377593bda574c31cd549f52d89219ab
deanqx <dean2@kowatsch.de>
2024-02-28 15:26:06 +0100

    Updated GIMP config and python lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -57,7 +57,8 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
 	ensure_installed = {
 		'rust_analyzer',
-        'jdtls'
+        'jdtls',
+        'pyright'
 	},
 	handlers = {
 		lsp.default_setup
