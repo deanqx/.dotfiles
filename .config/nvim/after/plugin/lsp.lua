36483631635e18c7158769488ebbb5ad3932daa4
deanqx <110404616+deanqx@users.noreply.github.com>
2024-08-20 12:42:36 +0200

    Fixed @tailwind is unknown

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -7,26 +7,26 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-	sources = {
-		{ name = 'nvim_lsp' },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    sources = {
+        { name = 'nvim_lsp' },
         { name = "nvim_lsp_signature_help" },
         { name = 'buffer' },
         { name = 'path' },
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-y>'] = cmp.mapping.complete(),
-		['<C-f>'] = cmp_action.luasnip_jump_forward(),
-		['<C-b>'] = cmp_action.luasnip_jump_backward(),
-		['<C-u>'] = cmp.mapping.scroll_docs(-4),
-		['<C-d>'] = cmp.mapping.scroll_docs(4),
-		['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-		['<C-d>'] = cmp.mapping.confirm({ select = true })
-	})
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-y>'] = cmp.mapping.complete(),
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(3),
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-v>'] = cmp.mapping.confirm({ select = true })
+    })
 })
 
 lsp.set_preferences({
@@ -40,35 +40,49 @@ lsp.set_preferences({
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "<leader>8", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>9", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-s>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "<leader>8", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>9", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-s>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = {
-		'rust_analyzer',
+    ensure_installed = {
+        'rust_analyzer',
         'jdtls',
         'pyright',
         'dockerls',
         'tailwindcss',
-        'tsserver'
-	},
-	handlers = {
-		lsp.default_setup
-	}
+        'tsserver',
+        'cssls',
+        'lua_ls',
+    },
+    handlers = {
+        lsp.default_setup
+    }
 })
+
+require('lspconfig').cssls.setup {
+    settings = {
+        css = {
+            validate = true,
+            lint = {
+                unknownAtRules = "ignore"
+            }
+        },
+    },
+}
+
 lsp.setup()
 
 vim.diagnostic.config({
