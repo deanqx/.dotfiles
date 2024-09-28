9a1bceef6173b40ed4ad1f1565c6a7f7c72b20d2
deanqx <110404616+deanqx@users.noreply.github.com>
2024-09-28 12:40:36 +0200

    Clean up, many keybinds from nvim were removed

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -18,11 +18,8 @@ cmp.setup({
         { name = 'path' },
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-y>'] = cmp.mapping.complete(),
         ['<C-f>'] = cmp_action.luasnip_jump_forward(),
         ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-d>'] = cmp.mapping.scroll_docs(3),
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         ['<C-v>'] = cmp.mapping.confirm({ select = true })
@@ -44,15 +41,13 @@ lsp.on_attach(function(client, bufnr)
 
     vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "<leader>8", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>9", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("i", "<C-s>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "<leader>k", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>j", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
 
     -- clangd formatting instead of prettier
     if client.server_capabilities.documentFormattingProvider and client.name ~= "null-ls" then
