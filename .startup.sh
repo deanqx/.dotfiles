094627a328d9c3cbd391ce1f9fb5677deb5f4d59
deanqx <110404616+deanqx@users.noreply.github.com>
2024-09-28 18:39:57 +0200

    Testing i3 Desktop, many things are still wrong

--- .startup.sh
+++ .startup.sh
@@ -1,4 +1,10 @@
-echo "hello" >> test.txt
+setxkbmap -option caps:escape
+
+if [ "$(whoami)" = "dean" ]; then
+    xrandr --output DP-4 --primary
+    xrandr --output DP-4 --right-of DP-0
+fi
+
 sleep 9
 nvidia-settings &
 sleep 1
