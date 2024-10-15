629e02ff306a4de925363de8ef08ba45cb8100e0
deanqx <110404616+deanqx@users.noreply.github.com>
2024-10-15 17:56:22 +0200

    Fixed prettier again

--- .config/nvim/after/plugin/prettier.lua
+++ .config/nvim/after/plugin/prettier.lua
@@ -4,14 +4,11 @@ prettier.setup({
   bin = 'prettier', -- or `'prettierd'` (v0.23.3+)
   filetypes = {
     "css",
-    "graphql",
     "html",
     "javascript",
     "javascriptreact",
     "json",
-    "less",
     "markdown",
-    "scss",
     "typescript",
     "typescriptreact",
     "yaml",
