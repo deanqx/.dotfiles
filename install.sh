afecb89841337cd2fb92d157484be8f5f4e9352e
deanqx <110404616+deanqx@users.noreply.github.com>
2024-10-16 19:09:43 +0200

    Updated after laptop installation

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+sudo pacman -S --needed git base-devel npm
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -r yay
+
+source update.sh
+
+sudo systemctl enable ly
+
+npm install -g prettier
+git submodule update --init --recursive
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
