5e9a587420fb55520864324182acb0288f9d027f
deanqx <110404616+deanqx@users.noreply.github.com>
2024-10-17 15:40:46 +0200

    Improved visibility of brackets

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -2,8 +2,9 @@ require('onedark').setup {
     transparent = true,
     style = 'dark',
     colors = {
-        grey = '#868686',
-        bg3 = '#1e1e2e' -- Selection
+        grey = '#868686', -- Comments
+        light_grey = '#a3abbf',
+        bg3 = '#1e1e2e'   -- Selection
     }
 }
-require('onedark').load() -- comment test
+require('onedark').load()
