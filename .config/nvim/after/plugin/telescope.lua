1f05fc8a4fe861a668a2384e957df6089a0e3a59
deanqx <110404616+deanqx@users.noreply.github.com>
2024-10-26 14:10:19 +0200

    Fixed telescope not finding all files

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,10 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.find_files({
+        hidden = true,     -- Search hidden files
+        follow = true,     -- Follow symlinks
+        no_ignore = false, -- Ignore .gitignore
+    })
+end, { desc = "Project Find Files" })
 vim.keymap.set('n', '<leader>ps', builtin.live_grep)
 vim.keymap.set('n', '<leader>pr', builtin.resume)
