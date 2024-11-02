fa82e07d5c4ca75328e61cea235980597db80c71
deanqx <110404616+deanqx@users.noreply.github.com>
2024-11-02 18:49:08 +0100

    Added treesitter to telescope

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -8,4 +8,4 @@ vim.keymap.set("n", "<leader>l", function() harpoon.ui:toggle_quick_menu(harpoon
 vim.keymap.set("n", "<leader>b", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<leader>o", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<leader>m", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader>t", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>i", function() harpoon:list():select(4) end)
