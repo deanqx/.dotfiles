a610a9a9c2586e4b5fb7f70a1f6517e621777e5a
deanqx <110404616+deanqx@users.noreply.github.com>
2025-05-26 18:34:12 +0200

    Reworked lsp config, not fully tested

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,41 +1,58 @@
-local lsp = require('lsp-zero')
-
-lsp.preset("recommended")
-
 local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_action = require('lsp-zero').cmp_action()
+local luasnip = require('luasnip')
+
+vim.diagnostic.config({
+    virtual_text = true
+})
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    automatic_enable = true,
+})
 
 cmp.setup({
     window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
     },
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
     sources = {
         { name = 'nvim_lsp' },
         { name = "nvim_lsp_signature_help" },
+        { name = 'luasnip' },
         { name = 'buffer' },
         { name = 'path' },
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-Tab>'] = cmp_action.luasnip_jump_forward(),
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        -- not tested
+        ['<C-n>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<C-p>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
         ['<C-v>'] = cmp.mapping.confirm({ select = true })
     })
 })
 
-lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
-})
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local format_group = vim.api.nvim_create_augroup("LspFormatting", {})
 
 function table.contains(table, element)
     for _, value in ipairs(table) do
@@ -46,76 +63,62 @@ function table.contains(table, element)
     return false
 end
 
-lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
 
-    vim.api.nvim_create_autocmd("BufWritePre", {
-        buffer = bufnr,
-        callback = function()
-            if vim.snippet then
-                vim.snippet.stop()
+-- is called every time a LSP attaches
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
+    callback = function(args)
+        local bufnr = args.buf
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        local opts = { buffer = bufnr, remap = false }
+
+        -- only set keymaps if LSP attaches
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        -- missing: vim.lsp.buf.type_definition(), vim.lsp.buf.implementation()
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("i", "<C-f>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<leader>k", function() vim.diagnostic.jump({ count = -1, float = true }) end, opts)
+        vim.keymap.set("n", "<leader>j", function() vim.diagnostic.jump({ count = 1, float = true }) end, opts)
+        vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
+
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            callback = function()
+                if vim.snippet then
+                    vim.snippet.stop()
+                end
             end
+        })
+
+        local current_filetype = vim.bo.filetype
+        local prettier_filetypes = { "javascript", "svelte", "typescriptreact", "typescript", "css", "html", "json" }
+
+        if table.contains(prettier_filetypes, current_filetype) then
+            vim.api.nvim_clear_autocmds({ group = format_group, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = format_group,
+                buffer = bufnr,
+                callback = function()
+                    vim.cmd('Prettier')
+                end
+            })
+        elseif client:supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = format_group, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = format_group,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.format({
+                        filter = function()
+                            return client.name ~= "ts_ls" and client.name ~= "cssls"
+                        end,
+                        bufnr = bufnr,
+                    })
+                end,
+            })
         end
-    })
-
-    vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("i", "<C-f>", function() vim.lsp.buf.signature_help() end, opts)
-    vim.keymap.set("n", "<leader>k", function() vim.diagnostic.jump({ count = 1, float = true }) end, opts)
-    vim.keymap.set("n", "<leader>j", function() vim.diagnostic.jump({ count = -1, float = true }) end, opts)
-    vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
-
-    local current_filetype = vim.bo.filetype
-    local prettier_filetypes = { "javascript", "svelte", "typescriptreact", "typescript", "css", "html", "json" }
-
-    if table.contains(prettier_filetypes, current_filetype) then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                vim.cmd('Prettier')
-            end
-        })
-    elseif client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                vim.lsp.buf.format({
-                    filter = function()
-                        return client.name ~= "ts_ls" and client.name ~= "cssls"
-                    end,
-                    bufnr = bufnr,
-                })
-            end,
-        })
     end
-end)
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    ensure_installed = {
-        'rust_analyzer',
-        'jdtls',
-        'pyright',
-        'dockerls',
-        'tailwindcss',
-        'cssls',
-        'lua_ls',
-        'clangd',
-    },
-    handlers = {
-        lsp.default_setup
-    }
-})
-
-lsp.setup()
-
-vim.diagnostic.config({
-    virtual_text = true
 })
