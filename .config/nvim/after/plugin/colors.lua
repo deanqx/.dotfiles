81dacf9ab9d96d72ea7e26c11b1121aae1d452da
deanqx <110404616+deanqx@users.noreply.github.com>
2025-06-10 20:12:08 +0200

    Terminal with black background

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -2,9 +2,11 @@ require('onedark').setup {
     transparent = true,
     style = 'dark',
     colors = {
+        -- bg0 = '#000000',  -- Background
+        bg2 = '#000000',  -- File name bar
+        bg3 = '#1e1e2e',  -- Selection
         grey = '#868686', -- Comments
         light_grey = '#a3abbf',
-        bg3 = '#1e1e2e'   -- Selection
     }
 }
 require('onedark').load()
