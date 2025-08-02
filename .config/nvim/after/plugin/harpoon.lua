9fe8dbc1561db19fe0c5b1b96998367065d5dd94
deanqx <110404616+deanqx@users.noreply.github.com>
2025-08-02 19:01:56 +0200

    Harpon keybinds have alphabetical order

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -6,6 +6,6 @@ vim.keymap.set("n", "<leader>h", function() harpoon:list():add() end, { desc = '
 vim.keymap.set("n", "<leader>l", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Harpoon: List' })
 
 vim.keymap.set("n", "<leader>b", function() harpoon:list():select(1) end, { desc = 'Harpoon: Goto 1' })
-vim.keymap.set("n", "<leader>o", function() harpoon:list():select(2) end, { desc = 'Harpoon: Goto 2' })
+vim.keymap.set("n", "<leader>i", function() harpoon:list():select(2) end, { desc = 'Harpoon: Goto 2' })
 vim.keymap.set("n", "<leader>m", function() harpoon:list():select(3) end, { desc = 'Harpoon: Goto 3' })
-vim.keymap.set("n", "<leader>i", function() harpoon:list():select(4) end, { desc = 'Harpoon: Goto 4' })
+vim.keymap.set("n", "<leader>o", function() harpoon:list():select(4) end, { desc = 'Harpoon: Goto 4' })
