76d40159ad97bdf5694731fb5fa60aec4e7f0562
deanqx <110404616+deanqx@users.noreply.github.com>
2025-10-21 12:44:02 +0200

    feat: prepare for nvim 11

--- .config/nvim/after/plugin/latex.lua
+++ .config/nvim/after/plugin/latex.lua
@@ -1,6 +1,4 @@
-local lspconfig = require("lspconfig")
-
-lspconfig.ltex.setup {
+vim.lsp.config.ltex = {
     cmd = { vim.fn.expand("~/.local/share/nvim/language_servers/ltex-ls-16.0.0/bin/ltex-ls") },
     filetypes = { "markdown", "tex", "text" }
 }
