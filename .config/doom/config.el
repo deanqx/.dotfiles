76fe7bf2ba31804c9fffbe05c0991d66a69fc56c
deanqx <dean2@kowatsch.de>
2026-08-12 22:44:34 +0200

    feat(emacs): transparent black background

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -4,13 +4,19 @@
 (setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 17 :weight 'semi-light)
       doom-variable-pitch-font (font-spec :family "JetBrainsMono Nerd Font" :size 17))
 
-;; Disable auto insert closing bracket
-(remove-hook 'doom-first-buffer-hook #'smartparens-global-mode)
+(setq doom-theme 'doom-one)
 
-(setq scroll-margin 8)
-(setq display-line-numbers-type 'relative)
-(after! evil
-  (setq +evil-want-o/O-to-continue-comments nil))
+(add-to-list 'default-frame-alist '(alpha-background . 97))
+
+(with-eval-after-load 'doom-themes
+  (custom-set-faces!
+    '(font-lock-comment-face :foreground "#999")
+    '(font-lock-doc-face :foreground "#aaa")
+    '(line-number :foreground "#666")
+    '(default :background "#000")
+    '(markdown-code-face :background "#1a1a1a")
+    '(magit-diff-context-highlight :background "#000")
+    '(solaire-default-face :background "#000")))
 
 ;; Always use block cursor
 (setq evil-normal-state-cursor   'box
@@ -21,7 +27,13 @@
       evil-operator-state-cursor 'box
       evil-emacs-state-cursor    'box)
 
-(setq doom-theme 'doom-one)
+
+(setq scroll-margin 8)
+(setq display-line-numbers-type 'relative)
+(after! evil
+  (setq +evil-want-o/O-to-continue-comments nil))
+;; Disable auto insert closing bracket
+(remove-hook 'doom-first-buffer-hook #'smartparens-global-mode)
 
 (setq org-directory "~/org/")
 
